2540f58870e11484f4b5cbc81e47e160ee21cf40
Thomas Röggla <t.roggla@cwi.nl>
2025-11-29 18:09:20 +0100

    added struct StreamType for representing type of stream

--- nl.cwi.dis.amplify/Runtime/AmplifyPortable/Redis/Stream.cs
+++ nl.cwi.dis.amplify/Runtime/AmplifyPortable/Redis/Stream.cs
@@ -1,5 +1,20 @@
 namespace AmplifyPortable.Redis
 {
+    public struct StreamType
+    {
+        public static readonly StreamType Discrete = new StreamType("discrete");
+        public static readonly StreamType Continuous = new StreamType("continuous");
+
+        private readonly string _type;
+
+        private StreamType(string type)
+        {
+            _type = type;
+        }
+
+        public override string ToString() => _type;
+    }
+
     public class Stream
     {
         public string Name { get; }
